d15fe6f3009be15452abf78d3b6dac073e254762
Shoya HARA <shioyarare@gmail.com>
2022-04-11 12:57:38 +0900

    fix: count method

--- main.rb
+++ main.rb
@@ -13,6 +13,18 @@ end
 # open file
 fname = ARGV[0]
 
+# Output 
+def writeCL (endpoint, data, val_min=2)
+  puts print "\e[31m#{endpoint}\e[0m"
+
+  data.each do |e|
+    if e[1] <= val_min then
+      next
+    end
+    puts "\t<#{e[1]}> - `#{e[0]}`"
+  end
+end
+
 File.open(fname, mode ="rt") { |f|
   tag = ""   # log tags
   rlogc = {} # Rails log counter
@@ -20,33 +32,36 @@ File.open(fname, mode ="rt") { |f|
 
   f.each_line{ |line|
 
-    if line == "\n" then next end
+    # dont read, skip
+    if line.include?("↳") then 
+      next
+    end
 
     # get current log tags
     curr_tag = line.scan(/(?<=\[).*?(?=\])/).slice(1).to_s.strip.chomp
 
-    if curr_tag == tag then
-      # extract query
-      target = /(?<=\)).*?(?=\[)/.match(line).to_s.strip.chomp
-      
-      if target.empty? then next end
-      rlogc[target] = rlogc.fetch(target, 0) + 1
-      
-    elsif line[0] == "I" then
-      # If initial char is 'I' and log tags chenged then exist tag.
-      tag = curr_tag
-      endpoint = /(?<=").*?(?=")/.match(line).to_s.strip.chomp
-      puts "#{endpoint}"
-      puts "a"
+    if curr_tag.empty? then next end
 
-      # Output
+    if line.include?("Started GET") or line.include?("Started POST") then
+      # Query start
+      endpoint = /(?<=").*?(?=")/.match(line).to_s.strip.chomp
+    elsif line.include?("Completed 200 OK") then
+      # Query end
+      
       res = rlogc.sort_by { |_, v| v}.reverse!
-      res.each do |e|
-        puts "\t<#{e[1]}> -- `#{e[0]}`"
-      end
+      writeCL(endpoint, res)
 
       # reset
       rlogc = {} 
+    else
+      # extract query
+      target = /(?<=\)).*?(?=\[)/.match(line).to_s.strip.chomp
+      
+        if target.empty? then 
+          target = /(?<=\)).*?$/.match(line).to_s.strip.chomp
+        end
+  
+        rlogc[target] = rlogc.fetch(target, 0) + 1
     end
   }
 }
